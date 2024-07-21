938cc3538e99c8c2c20f1828ef7e3456b0887573
shoaibamir <85135987+shoaibamir@users.noreply.github.com>
2024-07-21 12:45:34 +0000

    a

--- src/navigation.js
+++ src/navigation.js
@@ -27,6 +27,7 @@ export const headerData = {
     {
       text: 'Services',
       href: getPermalink('/services'),
+      
     },
     {
       text: 'Pricing',
@@ -192,7 +193,7 @@ export const footerData = {
   //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
   // ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/Pakistani_cargo' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/Pakistani_cargo' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/pakistanicargo' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/pakistancargouae' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
@@ -200,6 +201,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://www.pakistancargoexpress.com)]"></span>
-    <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://www.pakistanlogistics.com/"> Pakistan Cargo</a> · All rights reserved.
+    <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://www.pakistancargo.org/"> Pakistan Cargo</a> · All rights reserved.
   `,
 };
